0756b0f2f6b23232d261e59133ffe075b4fd10ba
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2026-02-25 12:24:28 +0900

    FUDG-031 : [Task] Prefabricate MenuScene buttons and manage them in UIController

--- Assets/Scripts/UI/MenuScene/MenuSceneUIController.cs
+++ Assets/Scripts/UI/MenuScene/MenuSceneUIController.cs
@@ -1,12 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.MenuScene
 {
     public class MenuSceneUIController : MonoBehaviour
     {
+        [Header("MenuButtons")]
+        public Button storyModeButton;
+        public Button challengeModeButton;
+        public Button tutorialButton;
+        public Button settingButton;
+        public Button quitButton;
+
         void Start()
         {
             UIManager.Instance.SetMenuSceneUIController(this);
+
+            storyModeButton.onClick.AddListener(OnStoryModeButtonClick);
+            challengeModeButton.onClick.AddListener(OnChallengeModeButtonClick);
+            tutorialButton.onClick.AddListener(OnTutorialModeButtonClick);
+            settingButton.onClick.AddListener(OnSettingModeButtonClick);
+            quitButton.onClick.AddListener(OnQuitModeButtonClick);
+        }
+
+        private void OnStoryModeButtonClick()
+        {
+            
+        }
+
+        private void OnChallengeModeButtonClick()
+        {
+
+        }
+
+        private void OnTutorialModeButtonClick()
+        {
+            
+        }
+
+        private void OnSettingModeButtonClick()
+        {
+            
+        }
+
+        private void OnQuitModeButtonClick()
+        {
+            GameManager.Instance.QuitGame();
         }
     }
 }
